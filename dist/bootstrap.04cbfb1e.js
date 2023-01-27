6af7869b2716ad8d8a123dbe74d671d34202d7cd
waldemar <vova108vova@gmail.com>
2023-01-27 19:01:07 +0300

    added glyphicons, call of pop-up consultation

--- dist/bootstrap.04cbfb1e.js
+++ dist/bootstrap.04cbfb1e.js
@@ -117,11 +117,62 @@ parcelRequire = (function (modules, cache, entry, globalName) {
   }
 
   return newRequire;
-})({"assets/css/bootstrap.css":[function(require,module,exports) {
+})({"../../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/bundle-url.js":[function(require,module,exports) {
+var bundleURL = null;
+function getBundleURLCached() {
+  if (!bundleURL) {
+    bundleURL = getBundleURL();
+  }
+  return bundleURL;
+}
+function getBundleURL() {
+  // Attempt to find the URL of the current script and use that as the base URL
+  try {
+    throw new Error();
+  } catch (err) {
+    var matches = ('' + err.stack).match(/(https?|file|ftp|chrome-extension|moz-extension):\/\/[^)\n]+/g);
+    if (matches) {
+      return getBaseURL(matches[0]);
+    }
+  }
+  return '/';
+}
+function getBaseURL(url) {
+  return ('' + url).replace(/^((?:https?|file|ftp|chrome-extension|moz-extension):\/\/.+)?\/[^/]+(?:\?.*)?$/, '$1') + '/';
+}
+exports.getBundleURL = getBundleURLCached;
+exports.getBaseURL = getBaseURL;
+},{}],"../../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/css-loader.js":[function(require,module,exports) {
+var bundle = require('./bundle-url');
+function updateLink(link) {
+  var newLink = link.cloneNode();
+  newLink.onload = function () {
+    link.remove();
+  };
+  newLink.href = link.href.split('?')[0] + '?' + Date.now();
+  link.parentNode.insertBefore(newLink, link.nextSibling);
+}
+var cssTimeout = null;
+function reloadCSS() {
+  if (cssTimeout) {
+    return;
+  }
+  cssTimeout = setTimeout(function () {
+    var links = document.querySelectorAll('link[rel="stylesheet"]');
+    for (var i = 0; i < links.length; i++) {
+      if (bundle.getBaseURL(links[i].href) === bundle.getBundleURL()) {
+        updateLink(links[i]);
+      }
+    }
+    cssTimeout = null;
+  }, 50);
+}
+module.exports = reloadCSS;
+},{"./bundle-url":"../../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/bundle-url.js"}],"assets/css/bootstrap.css":[function(require,module,exports) {
 var reloadCSS = require('_css_loader');
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{}],"../../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"./..\\fonts\\glyphicons-halflings-regular.eot":[["glyphicons-halflings-regular.69a446f5.eot","assets/fonts/glyphicons-halflings-regular.eot"],"assets/fonts/glyphicons-halflings-regular.eot"],"./..\\fonts\\glyphicons-halflings-regular.woff2":[["glyphicons-halflings-regular.14e42f41.woff2","assets/fonts/glyphicons-halflings-regular.woff2"],"assets/fonts/glyphicons-halflings-regular.woff2"],"./..\\fonts\\glyphicons-halflings-regular.woff":[["glyphicons-halflings-regular.7feedb01.woff","assets/fonts/glyphicons-halflings-regular.woff"],"assets/fonts/glyphicons-halflings-regular.woff"],"./..\\fonts\\glyphicons-halflings-regular.ttf":[["glyphicons-halflings-regular.29a174b8.ttf","assets/fonts/glyphicons-halflings-regular.ttf"],"assets/fonts/glyphicons-halflings-regular.ttf"],"./..\\fonts\\glyphicons-halflings-regular.svg":[["glyphicons-halflings-regular.f7f96281.svg","assets/fonts/glyphicons-halflings-regular.svg"],"assets/fonts/glyphicons-halflings-regular.svg"],"_css_loader":"../../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -146,7 +197,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "65309" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58522" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
